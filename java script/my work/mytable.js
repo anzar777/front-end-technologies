85fdb7b84516569fc0ebfc773a9a030587ecaa61
anzar777 <167102711+anzar777@users.noreply.github.com>
2024-04-21 19:59:13 +0530

    Add files via upload

---
+++ java script/my work/mytable.js
@@ -0,0 +1,20 @@
+function PrintAll(){
+   var name= document.getElementById("firstname").Value
+        document.getElementById("first").innerHTML="this is your first  name "+name
+        var lastname= document.getElementById("lastname").Value
+        document.getElementById("last").innerHTML="this is your last name mr "+lastname
+        var dob=document.getElementById("dob").Value
+        document.getElementById("birth").innerHTML="birth "+dob
+        var email=document.getElementById("email").Value
+        document.getElementById("gmail").innerhtml="mail"+email
+        var phone=document.getElementById("phone").Value
+        document.getElementById("call").innerHTML="call "+phone
+        var address=document.getElementById("address").Value
+        document.getElementById("adress").innerhtml="this your address "+address
+        var city=document.getElementById("city").Value
+        document.getElementById("place").innerHTML="this is your state "+city
+        var state=document.getElementById("state").Value
+        document.getElementById("sta").innerHTML="this is your state "+state
+        var pin=document.getElementById("pin").Value
+        document.getElementById("code").innerHTML="this is your pin code"pin
+}
